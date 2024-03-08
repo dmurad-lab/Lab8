d8c76ab9c980447a6a4cecfaa987d0e4f3db6a87
Daniel Murad <dmurad@ualberta.ca>
2024-03-08 15:36:08 -0700

    add

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -8,14 +8,25 @@ import androidx.collection.ArraySet;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
+import java.util.ArrayList;
+
 
 public class CustomListTest {
     private City city;
     private CustomList cityList;
+    private CustomList customList(Object o, ArrayList<> arrayList) {
+    }
+
 
     @Test
     public void addCityTest(){
+        CustomList list = customList(null, new ArrayList<>());
+        int listSize = list.getCount();
+        list.addCity(new City("Uttara", "Dhaka"));
+        assertEquals(list.getCount(),listSize + 1);
     }
+
+
 }
 
 
